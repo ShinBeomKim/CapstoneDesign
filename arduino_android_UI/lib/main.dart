c21cecb5bd05c4b3ecb3a3be5c33c7c2c2db1df9
ShinBeomKim <106213869+ShinBeomKim@users.noreply.github.com>
2023-04-14 00:50:37 +0900

    Add files via upload

--- arduino_android_UI/lib/main.dart
+++ arduino_android_UI/lib/main.dart
@@ -1,74 +1,97 @@
 import 'package:flutter/material.dart';
-import 'Image_Screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-        appBar: AppBar(
-        title: Text('Smart Loker'),
-    centerTitle: true,
-    backgroundColor: Colors.black,
-    actions: [
-    IconButton(
-    icon: Icon(Icons.account_circle),
-    onPressed: () {
-    print('account clicked');
-    },
-    )
-    ],
-    ),
-    body: Container(
-    margin: EdgeInsets.all(20),
-    child: GestureDetector(
-    onTapDown: (TapDownDetails details) {
-    final RenderBox box = context.findRenderObject() as RenderBox;
-    final Offset localOffset = box.globalToLocal(details.globalPosition);
-    final Size size = box.size;
-    final double x = localOffset.dx / size.width;
-    final double y = localOffset.dy / size.height;
-    if (x >= 0.3 && x <= 0.7 && y >= 0.3 && y <= 0.7) {
-    Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => ImageScreen()),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlutterLogo(size: 100),
+            SizedBox(height: 16),
+            Text(
+              '로그인 화면',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 16),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 32),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: '아이디',
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 32),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: '비밀번호',
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // 로그인 버튼 클릭 시 메인 화면으로 이동
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => MainPage()),
+                );
+              },
+              child: Text('로그인'),
+            ),
+          ],
+        ),
+      ),
     );
-    }
-    child: InteractiveViewer(
-    child: Image.asset(
-    'assets/images/locker.png',
-    fit: BoxFit.contain,
-    ),
-    ),
-    ),
-    ),
-    bottomNavigationBar: BottomAppBar(
-    child: Container(
-    height: 60,
-    child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-    Icon(Icons.lock),
-    Icon(Icons.place),
-    Icon(Icons.more_horiz),
-    ],
-    ),
-    ),
-    ),
-    floatingActionButton: FloatingActionButton(
-    onPressed: () {
-    print('FloatingActionButton clicked');
-    },
-    child: Icon(Icons.add),
-    ),
-    floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    ),
-    );
-    }
   }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Smart Loker'),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.account_circle),
+            onPressed: () {
+              print('account clicked');
+            },)
+        ],),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/map.jpg'),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.lock),
+              Icon(Icons.place),
+              Icon(Icons.more_horiz),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: LoginPage(),
+  ));
+}
