d0a636a3ade8f087fef5f23ebe3b271f3dd95cf4
UnusedPig <npc_user@naver.com>
2023-04-14 10:50:18 +0900

    지도의 가운데 버튼 클릭시 새로운 화면으로 전환

--- arduino_android_UI/lib/main.dart
+++ arduino_android_UI/lib/main.dart
@@ -11,64 +11,67 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
+      home: Scaffold(
         appBar: AppBar(
-        title: Text('Smart Loker'),
-    centerTitle: true,
-    backgroundColor: Colors.black,
-    actions: [
-    IconButton(
-    icon: Icon(Icons.account_circle),
-    onPressed: () {
-    print('account clicked');
-    },
-    )
-    ],
-    ),
-    body: Container(
-    margin: EdgeInsets.all(20),
-    child: GestureDetector(
-    onTapDown: (TapDownDetails details) {
-    final RenderBox box = context.findRenderObject() as RenderBox;
-    final Offset localOffset = box.globalToLocal(details.globalPosition);
-    final Size size = box.size;
-    final double x = localOffset.dx / size.width;
-    final double y = localOffset.dy / size.height;
-    if (x >= 0.3 && x <= 0.7 && y >= 0.3 && y <= 0.7) {
-    Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => ImageScreen()),
+          title: const Text('Smart Loker'),
+          centerTitle: true,
+          backgroundColor: Colors.black,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.account_circle),
+              onPressed: () {
+                print('account clicked');
+              },
+            )
+          ],
+        ),
+        body: Builder(
+          builder: (context) => GestureDetector(
+            onTapDown: (TapDownDetails details) {
+              final RenderBox box = context.findRenderObject() as RenderBox;
+              final Offset localOffset = box.globalToLocal(details.globalPosition);
+              final Size size = box.size;
+              final double x = localOffset.dx / size.width;
+              final double y = localOffset.dy / size.height;
+              if (x >= 0.3 && x <= 0.7 && y >= 0.3 && y <= 0.7) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ImageScreen()),
+                );
+              }
+            },
+            child: Container(
+              margin: const EdgeInsets.all(20),
+              child: InteractiveViewer(
+                child: Image.asset(
+                  'assets/images/locker.png',
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            height: 60,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Icon(Icons.lock),
+                const Icon(Icons.place),
+                const Icon(Icons.more_horiz),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('FloatingActionButton clicked');
+          },
+          child: const Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      ),
     );
-    }
-    child: InteractiveViewer(
-    child: Image.asset(
-    'assets/images/locker.png',
-    fit: BoxFit.contain,
-    ),
-    ),
-    ),
-    ),
-    bottomNavigationBar: BottomAppBar(
-    child: Container(
-    height: 60,
-    child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-    Icon(Icons.lock),
-    Icon(Icons.place),
-    Icon(Icons.more_horiz),
-    ],
-    ),
-    ),
-    ),
-    floatingActionButton: FloatingActionButton(
-    onPressed: () {
-    print('FloatingActionButton clicked');
-    },
-    child: Icon(Icons.add),
-    ),
-    floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    ),
-    );
-    }
   }
+}
